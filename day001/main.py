fc768921e78d06ce6b0c8647a284571b1c65053e
DJBostock <douglasjbostock@gmail.com>
2022-07-15 13:58:27 -0500

    end of lesson 1 Band Name Generator

--- day001/main.py
+++ day001/main.py
@@ -1,1 +1,6 @@
-print("Hello, World!")
+# Band Name Generator
+
+print("Welcome to the Band Name Generator.")
+street = input("What is the name of the city in which you grew up?\n")
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
