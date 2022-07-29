7c90380bc7e0277037c861e9127984b3f5a9d1e8
DJBostock <douglasjbostock@gmail.com>
2022-07-29 06:49:36 -0500

    day 8 functions with inputs lesson

---
+++ day008_caesar_cipher/functions_with_inputs.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello.")
+    print("How are you?")
+    print("How is the weather?")
+
+
+def greet_with_name(name):
+    print(f"Hello, {name}")
+    print(f"How are you, {name}?")
+    print(f"{name}, how is the weather?")
+
+
+greet()
+greet_with_name("DJ")
